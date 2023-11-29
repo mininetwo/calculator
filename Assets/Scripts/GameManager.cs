0f4228f4948e8476e282a2ac7d0d7e8751c9a7eb
Mingoo Park <sogoodmg@gmail.com>
2023-11-29 13:19:01 +0900

    GameManager Switch문 추가

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,141 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // 계산기 디스플레이
+    public TMP_Text displayText;
 
-    // Update is called once per frame
-    void Update()
+    // 버튼 클릭 이벤트
+    public void OnClickButton(string buttonStr)
     {
-        
+        switch (buttonStr)
+        {
+            case "0":
+                if (displayText.text == "0")
+                {
+                    displayText.text = buttonStr;
+                }
+                else
+                {
+                    displayText.text += buttonStr;
+                }
+                break;
+            case "1":
+                if (displayText.text == "0")
+                {
+                    displayText.text = buttonStr;
+                }
+                else
+                {
+                    displayText.text += buttonStr;
+                }
+                break;
+            case "2":
+                if (displayText.text == "0")
+                {
+                    displayText.text = buttonStr;
+                }
+                else
+                {
+                    displayText.text += buttonStr;
+                }
+                break;
+            case "3":
+                if (displayText.text == "0")
+                {
+                    displayText.text = buttonStr;
+                }
+                else
+                {
+                    displayText.text += buttonStr;
+                }
+                break;
+            case "4":
+                if (displayText.text == "0")
+                {
+                    displayText.text = buttonStr;
+                }
+                else
+                {
+                    displayText.text += buttonStr;
+                }
+                break;
+            case "5":
+                if (displayText.text == "0")
+                {
+                    displayText.text = buttonStr;
+                }
+                else
+                {
+                    displayText.text += buttonStr;
+                }
+                break;
+            case "6":
+                if (displayText.text == "0")
+                {
+                    displayText.text = buttonStr;
+                }
+                else
+                {
+                    displayText.text += buttonStr;
+                }
+                break;
+            case "7":
+                if (displayText.text == "0")
+                {
+                    displayText.text = buttonStr;
+                }
+                else
+                {
+                    displayText.text += buttonStr;
+                }
+                break;
+            case "8":
+                if (displayText.text == "0")
+                {
+                    displayText.text = buttonStr;
+                }
+                else
+                {
+                    displayText.text += buttonStr;
+                }
+                break;
+            case "9":
+                if (displayText.text == "0")
+                {
+                    displayText.text = buttonStr;
+                }
+                else
+                {
+                    displayText.text += buttonStr;
+                }
+                break;
+            case ".":
+                if (displayText.text == "0")
+                {
+                    displayText.text = buttonStr;
+                }
+                else
+                {
+                    displayText.text += buttonStr;
+                }
+                break;
+            case "c":
+                break;
+            case "+":
+                break;
+            case "-":
+                break;
+            case "*":
+                break;
+            case "/":
+                break;
+            case "=":
+                break;
+
+        }
     }
 }
