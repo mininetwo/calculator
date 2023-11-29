dcb5e1d8a5f3220c25e79d81e7c9901edd380673
Mingoo Park <sogoodmg@gmail.com>
2023-11-29 15:21:42 +0900

    계산기 생성 완료

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,102 +8,42 @@ public class GameManager : MonoBehaviour
     // 계산기 디스플레이
     public TMP_Text displayText;
 
+    // 연산자를 담을 변수
+    private List<string> operators = new List<string>();
+
+    // 피연산자를 담을 변수
+    private List<string> operands = new List<string>();
+
+    // 연산자를 입력했을때 해당 연산자와 이전 피연산자 저장을 위한 변수
+    private string lastOperand;
+    private string lastOperator;
+
     // 버튼 클릭 이벤트
     public void OnClickButton(string buttonStr)
     {
         switch (buttonStr)
         {
             case "0":
-                if (displayText.text == "0")
-                {
-                    displayText.text = buttonStr;
-                }
-                else
-                {
-                    displayText.text += buttonStr;
-                }
-                break;
             case "1":
-                if (displayText.text == "0")
-                {
-                    displayText.text = buttonStr;
-                }
-                else
-                {
-                    displayText.text += buttonStr;
-                }
-                break;
             case "2":
-                if (displayText.text == "0")
-                {
-                    displayText.text = buttonStr;
-                }
-                else
-                {
-                    displayText.text += buttonStr;
-                }
-                break;
             case "3":
-                if (displayText.text == "0")
-                {
-                    displayText.text = buttonStr;
-                }
-                else
-                {
-                    displayText.text += buttonStr;
-                }
-                break;
             case "4":
-                if (displayText.text == "0")
-                {
-                    displayText.text = buttonStr;
-                }
-                else
-                {
-                    displayText.text += buttonStr;
-                }
-                break;
             case "5":
-                if (displayText.text == "0")
-                {
-                    displayText.text = buttonStr;
-                }
-                else
-                {
-                    displayText.text += buttonStr;
-                }
-                break;
             case "6":
-                if (displayText.text == "0")
-                {
-                    displayText.text = buttonStr;
-                }
-                else
-                {
-                    displayText.text += buttonStr;
-                }
-                break;
             case "7":
-                if (displayText.text == "0")
-                {
-                    displayText.text = buttonStr;
-                }
-                else
-                {
-                    displayText.text += buttonStr;
-                }
-                break;
             case "8":
-                if (displayText.text == "0")
-                {
-                    displayText.text = buttonStr;
-                }
-                else
-                {
-                    displayText.text += buttonStr;
-                }
-                break;
             case "9":
+                if (lastOperator != null && lastOperand != null)
+                {
+                    operands.Add(lastOperand);
+                    operators.Add(lastOperator);
+
+                    lastOperator = null;
+                    lastOperand = null;
+
+                    displayText.text = "0";
+                }
+
                 if (displayText.text == "0")
                 {
                     displayText.text = buttonStr;
@@ -114,28 +54,89 @@ public class GameManager : MonoBehaviour
                 }
                 break;
             case ".":
-                if (displayText.text == "0")
-                {
-                    displayText.text = buttonStr;
-                }
-                else
-                {
-                    displayText.text += buttonStr;
-                }
+                displayText.text += buttonStr;
                 break;
             case "c":
+                displayText.text = "0";
+                operands.Clear();
+                operators.Clear();
                 break;
             case "+":
-                break;
             case "-":
-                break;
             case "*":
-                break;
             case "/":
+                lastOperand = displayText.text;
+                lastOperator = buttonStr;
                 break;
             case "=":
-                break;
+                operands.Add(displayText.text);
 
+                while (operators.Count > 0)
+                {
+                    int multiplyIndex = operators.IndexOf("*");
+                    int divideIndex = operators.IndexOf("/");
+                    int plusIndex = operators.IndexOf("+");
+                    int minusIndex = operators.IndexOf("-");
+                    
+                    if (multiplyIndex >= 0)
+                    {
+                        DoCalculate(multiplyIndex, OPR.MULTIPLY);
+                    }
+                    else if (divideIndex >= 0)
+                    {
+                        DoCalculate(divideIndex, OPR.DIVIDE);
+                    }
+                    else if (plusIndex >= 0)
+                    {
+                        DoCalculate(plusIndex, OPR.PLUS);
+                    }
+                    else if (minusIndex >= 0)
+                    {
+                        DoCalculate(minusIndex, OPR.MINUS);
+                    }
+                }
+
+                displayText.text = operands[0];
+                operands.Clear();
+
+                break;
         }
+    }
+
+    enum OPR { PLUS, MINUS, MULTIPLY, DIVIDE };
+
+    private void DoCalculate(int index, OPR opr)
+    {
+        float leftOperand = float.Parse(operands[index]);
+        float rightOperand = float.Parse(operands[index + 1]);
+
+        float result;
+
+        switch (opr)
+        {
+            case OPR.PLUS:
+                result = leftOperand + rightOperand;
+                Debug.Log($"{leftOperand} + {rightOperand} : {result}");
+                break;
+            case OPR.MINUS:
+                result = leftOperand - rightOperand;
+                Debug.Log($"{leftOperand} - {rightOperand} : {result}");
+                break;
+            case OPR.MULTIPLY:
+                result = leftOperand * rightOperand;
+                Debug.Log($"{leftOperand} * {rightOperand} : {result}");
+                break;
+            case OPR.DIVIDE:
+                result = leftOperand / rightOperand;
+                Debug.Log($"{leftOperand} / {rightOperand} : {result}");
+                break;
+            default:
+                result = 0;
+                break;
+        }
+
+        operands[index] = result.ToString();
+        operands.RemoveAt(index + 1);
+        operators.RemoveAt(index);
     }
 }
